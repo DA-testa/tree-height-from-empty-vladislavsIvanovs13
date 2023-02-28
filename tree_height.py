07644335b8cdf7fdf9773e99adfde9a17f3f3f4f
vladIvanov13 <124610956+vladislavsIvanovs13@users.noreply.github.com>
2023-02-28 20:30:21 +0200

    Add files via upload

--- tree_height.py
+++ tree_height.py
@@ -2,6 +2,7 @@
 
 import sys
 import threading
+import numpy
 
 
 def compute_height(n, parents):
@@ -20,7 +21,7 @@ def main():
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
-
+    pass
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
@@ -28,3 +29,5 @@ def main():
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
+main()
+# print(numpy.array([1,2,3]))
