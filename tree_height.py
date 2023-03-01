fa72b0de4d22ba5ed1aa89afc913978193724bb0
vladIvanov13 <124610956+vladislavsIvanovs13@users.noreply.github.com>
2023-03-02 00:33:44 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -6,14 +6,33 @@ import numpy
 
 
 def compute_height(n, parents):
-    # Write this function
     max_height = 0
-    # Your code here
+    for i in range(n):
+        counter = 1
+        number = parents[i]
+        while not (number == -1):
+            counter = counter + 1;
+            number = parents[i]
+        max_height = max(max_height, counter)
     return max_height
 
 
 def main():
-    # implement input form keyboard and from files
+    test = input()
+    if "F" in test:
+        file_name = input()
+        if not "a" in file_name:
+            #activate test
+            pass
+        
+        
+    if "I" in test:
+        n = input()
+        parents = input()
+        arr = parents.split(" ")
+        arr = numpy.array(arr)
+        print(compute_height(n, arr))
+        
     
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
@@ -21,7 +40,6 @@ def main():
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
-    pass
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
