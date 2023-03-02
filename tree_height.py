2a5f34c7b495eed21cbb711e7c9b7309164d62ba
vladIvanov13 <124610956+vladislavsIvanovs13@users.noreply.github.com>
2023-03-02 22:42:24 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -24,7 +24,7 @@ def main():
     if "F" in command:
         file_name = input()
         if not "a" in file_name:
-            contents = __init__(file_name)
+            contents = __import__(file_name)
             print(contents)
         
         
