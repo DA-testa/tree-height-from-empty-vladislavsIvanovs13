fc19529d7fd55555b55b064c54552b3314f30ecc
vladIvanov13 <124610956+vladislavsIvanovs13@users.noreply.github.com>
2023-03-04 20:47:36 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -38,7 +38,8 @@ def main():
             n = int(partitioned[0])
             arr = partitioned[2].split(" ")
             arr = numpy.array(arr)
-            print(compute_height(n, arr))
+            with recursion_depth(10**7):
+                print(compute_height(n, arr))
         
         
     elif "I" in command:
@@ -46,7 +47,8 @@ def main():
         parents = input()
         arr = parents.split(" ")
         arr = numpy.array(arr)
-        print(compute_height(n, arr))
+        with recursion_depth(10**7):
+            print(compute_height(n, arr))
         
     
     # let user input file name to use, don't allow file names with letter a
@@ -59,8 +61,8 @@ def main():
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
+# sys.setrecursionlimit(10**7)  # max depth of recursion
+# threading.stack_size(2**27)   # new thread will get stack of such size
 # threading.Thread(target=main).start()
 main()
 # print(numpy.array([1,2,3]))
